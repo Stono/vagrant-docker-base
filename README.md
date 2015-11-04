8d96f6b8da721b62c7312314eb676a79020bb9af
Karl Stoney <kstoney@thoughtworks.com>
2015-11-04 10:52:12 +0000

    Yum updated, and docker-toolbox installation

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vagrant Doker Base Box
+This repository will build a Vagrant box for the VirtualBox provider, with docker-toolbox installed and ready for use.
---
+++ scripts/docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cat >/etc/yum.repos.d/docker.repo <<-EOF
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+yum -y install docker-engine
+yum -y clean all
+
+service docker start
+chkconfig docker on
