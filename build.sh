4bc3a7c5b878da96cca60ac0f896dac1ca8151e9
Karl Stoney <kstoney@thoughtworks.com>
2015-11-04 11:17:36 +0000

    Removed the build script as didnt realise that packer iso_url is cached

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ ! -f "CentOS-7-x86_64-Minimal-1503-01.iso" ]; then
-  echo Centos7 ISO not found, downloading...
-  wget http://mirrors.kernel.org/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1503-01.iso
-else 
-  echo Centos7 ISO found!
-fi
-
-packer build packer.json
